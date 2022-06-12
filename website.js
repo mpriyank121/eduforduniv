6ff3759a112c21dd2bf321bf969c09b5b97743ea
mpriyank121 <86019596+mpriyank121@users.noreply.github.com>
2022-06-12 14:02:35 +0530

    first commit

---
+++ website.js
@@ -0,0 +1,24 @@
+
+function min() {
+  document.querySelector('#btn').style.backgroundColor = ' #e60000';
+  document.querySelector('#btn').style.color = 'white';
+  document.querySelector('#btn').style.border = '0px';
+}
+
+function mout() {
+  document.querySelector('#btn').style.backgroundColor = '';
+  document.querySelector('#btn').style.color = 'black';
+}
+
+
+function main() {
+  document.querySelector('#btnab').style.backgroundColor = '#e60000';
+  document.querySelector('#btnab').style.color = 'white';
+document.querySelector('#btnab').style.border = '0px';
+  }
+
+function maout() {
+  document.querySelector('#btnab').style.backgroundColor = '';
+  document.querySelector('#btnab').style.color = 'black';
+
+}
